e9dff926df7c9d3ec04450780bdfaa259ca3a268
leezter <66684146+leezter@users.noreply.github.com>
2025-05-08 16:50:58 +0200

    Defined a book model
    created the author and book tables in the database

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
-from flask import flask
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from data_models import db, Author, Book 
+import os
+
+app = Flask(__name__)
+
+# Update the database URI to use an absolute path
+base_dir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(base_dir, "data", "library.sqlite")}'
+
+db.init_app(app)
+
+
+""" run one time to create the database tables """
+# with app.app_context():
+#     db.create_all()
+
+
 
--- data_models.py
+++ data_models.py
@@ -10,9 +10,29 @@ class Author(db.Model):
     birth_date = db.Column(db.Date, nullable=True)
     date_of_death = db.Column(db.Date, nullable=True)
 
+
     def __repr__(self):
         return f"<Author(id={self.id}, name='{self.name}')>"
 
+
     def __str__(self):
         return f"Author: {self.name}"
+
+
+class Book(db.Model):
+    __tablename__ = "books"
+    id = db.Column(db.Integer, primary_key=True)
+    isbn = db.Column(db.String(13), nullable=False, unique=True)
+    title = db.Column(db.String(200), nullable=False)
+    publication_year = db.Column(db.Integer, nullable=True)
+    author_id = db.Column(db.Integer, db.ForeignKey('authors.id'), nullable=False)
+
+
+    def __repr__(self):
+        return f"<Book(id={self.id}, title='{self.title}', isbn='{self.isbn}')>"
+
+
+    def __str__(self):
+        return f"Book: {self.title} (ISBN: {self.isbn})"
+
 
