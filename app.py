1fcd9c784c1d8c4202846f4ef6b8dc67499ec1ac
leezter <66684146+leezter@users.noreply.github.com>
2025-05-08 17:35:28 +0200

    defined logic for adding authors to the library

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from data_models import db, Author, Book 
 import os
@@ -15,6 +15,23 @@ db.init_app(app)
 """ run one time to create the database tables """
 # with app.app_context():
 #     db.create_all()
+
+@app.route('/add_author', methods=['GET', 'POST'])
+def add_author():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        birth_date = request.form.get('birthdate')
+        date_of_death = request.form.get('date_of_death')
+
+        if name:
+            new_author = Author(name=name, birth_date=birth_date, date_of_death=date_of_death)
+            db.session.add(new_author)
+            db.session.commit()
+            flash('Author added successfully!', 'success')
+        else:
+            flash('Name is required!', 'error')
+
+    return render_template('add_author.html')
 
 
 
